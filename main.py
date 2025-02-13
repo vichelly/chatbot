a7bd1be6313abd13c7818c0af8cb77d3b421a188
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-02-12 21:07:09 -0300

    IA treinada

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ myChatBot = ChatBot()
 
 
 #apenas carregar um modelo pronto
-#myChatBot.loadModel()
+myChatBot.loadModel()
 
 #criar o modelo
-myChatBot.createModel()
+#myChatBot.createModel()
 
 
 
@@ -16,13 +16,13 @@ myChatBot.createModel()
 print("Bem vindo ao Chatbot")
 
 
-pergunta = input("como posso te ajudar?")
+pergunta = input("como posso te ajudar?\n")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
 print(resposta + "   ["+intencao[0]['intent']+"]")
 
 
 while (intencao[0]['intent']!="despedida"):
-    pergunta = input("posso lhe ajudar com algo a mais?")
+    pergunta = input("posso lhe ajudar com algo a mais?\n")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
     print(resposta + "   [" + intencao[0]['intent'] + "]")
 
